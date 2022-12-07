c33cb688ce6a584bcd0a97be2cf8974c3691f07f
ThanhLuyen2212 <91460495+ThanhLuyen2212@users.noreply.github.com>
2022-12-07 09:38:16 +0700

    IIterator trong Giỏ hàng lần 1

---
+++ MauThietKePhanMem/Controllers/GioHangKhachLeIIteratorController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MauThietKePhanMem.Models;
+
+namespace MauThietKePhanMem.Controllers
+{
+    public class GioHangKhachLeIIteratorController : Controller
+    {
+        MauThietKePhanMemEntities data = new MauThietKePhanMemEntities();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GioHangKhachLe
+        public GioHang GetHang()
+        {
+            GioHang gio = Session["GioHang"] as GioHang;
+            if ((gio == null) || Session["GioHang"] == null)
+            {
+                gio = new GioHang();
+                Session["GioHang"] = gio;
+            }
+            return gio;
+        }
+
+
+        public ActionResult Addto(string id)
+        {
+            var gio = data.MatHangs.SingleOrDefault(s => s.IDMH.ToString() == id);
+            if (gio != null)
+            {
+                GetHang().Add(gio);
+
+                // Số lượng hàng trong giở là bao nhiêu
+                GioHang gioHang = Session["GioHang"] as GioHang;
+                Session["SoLuongHangTrongGioHang"] = gioHang.sum().ToString();
+            }
+            return RedirectToAction("Show", "GioHangKhachLeIIterator");
+        }
+
+        public ActionResult Show()
+        {
+            if (Session["GioHang"] == null)
+            {
+                return RedirectToAction("Index", "Shop");
+            }
+
+            /*KhachHang kh = (KhachHang)Session["KhachHang"];*/
+
+            GioHang gio = Session["GioHang"] as GioHang;
+
+            return View(gio);
+        }
+
+        public ActionResult Update_quantity(FormCollection form)
+        {
+            GioHang gio = Session["GioHang"] as GioHang;
+            string id_MatHang = form["ID MatHang"];
+            int quatity = int.Parse(form["quantity"]);
+
+            gio.Update_quantity(id_MatHang, quatity);
+
+            Session["SoLuongHangTrongGioHang"] = gio.sum().ToString();
+
+            return RedirectToAction("Show", "GioHangKhachLeIIterator");
+
+        }
+
+        public ActionResult Remove(int id)
+        {
+            GioHang gio = Session["GioHang"] as GioHang;
+            gio.Remove(id);
+
+            Session["SoLuongHangTrongGioHang"] = gio.sum().ToString();
+            return RedirectToAction("Show", "GioHangKhachLeIIterator");
+        }
+
+
+        public PartialViewResult BagMathang()
+        {
+            int total = 0;
+            GioHang gio = Session["GioHang"] as GioHang;
+            if (gio != null)
+            {
+                total = gio.Total();
+            }
+
+            ViewBag.InforGio = total;
+            return PartialView("BagMatHang");
+        }
+
+        public ActionResult Mua_Success()
+        {
+            return RedirectToAction("Index", "PhongThuThanhToan");
+        }
+
+        [HttpPost]
+        public ActionResult MuaHang(FormCollection form)
+        { 
+            try
+            {
+                // Lấy tưng sản phẩm
+                GioHang gio = Session["GioHang"] as GioHang;
+
+                //List<Hang> gio = Session["GioHang"] as List<Hang>;
+
+                double _tongHang = 0;
+
+                GioHangIIterator giohang = new GioHangIIterator(gio.ListHang.ToList());
+                _tongHang =  giohang.sum();
+
+
+                foreach (var item in gio.ListHang)
+                {
+                    if (item._soLuongHang <= 0)
+                    {
+                        gio.Remove(item.gioHang.IDMH);
+                    }
+                    //_tongHang += item._soLuongHang;
+
+                    if (_tongHang == 0)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    MatHang mh = data.MatHangs.Find(item.gioHang.IDMH);
+                    if (item._soLuongHang > mh.SoLuong)
+                    {
+                        gio.Remove(item.gioHang.IDMH);
+                        return Content("Số lượng mặt hàng " + mh.TenMH + " không đủ!");
+                    }
+
+                }
+                Session["GioHang"] = gio;
+
+                return RedirectToAction("XacNhan", "XacNhanDonHangKhachLe");
+            }
+            catch
+            {
+                return Content("Vui lòng kiểm tra lại thông tin!");
+            }
+
+        }
+    }
+}
---
+++ MauThietKePhanMem/Models/GioHangIIterator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MauThietKePhanMem.Models
+{
+
+    //public class Hang
+    //{
+    //    public MatHang gioHang { get; set; }
+    //    public int _soLuongHang { get; set; }
+
+    //}
+
+    public interface IIterator
+    {
+        int TongTien();
+
+        int Total();
+
+        double sum();
+    }
+
+
+    public class GioHangIIterator : IIterator
+    {
+
+        public List<Hang> _listHang { get; } = new List<Hang>();
+               
+        public GioHangIIterator(List<Hang> listHang)
+        {
+            _listHang = listHang;
+        }
+
+
+       /* List<Hang> listHang = new List<Hang>();*/
+
+        public IEnumerable<Hang> ListHang
+        {
+            get { return _listHang; }
+        }
+
+      
+        public void Add(MatHang _matHang, int _soLuong = 1)
+        {
+            var i = _listHang.FirstOrDefault(s => s.gioHang.IDMH == _matHang.IDMH);
+            if (i == null)
+            {
+                _listHang.Add(new Hang
+                {
+                    gioHang = _matHang,
+                    _soLuongHang = _soLuong
+                });
+            }
+            else
+            {
+                i._soLuongHang += _soLuong;
+            }
+        }
+
+        public void Update_quantity(string id, int _quatity)
+        {
+            var i = _listHang.Find(s => s.gioHang.IDMH.ToString() == id);
+            if (i != null)
+            {
+                i._soLuongHang = _quatity;
+            }
+        }
+
+        public double sum()
+        {
+            var sum = _listHang.Sum(s => s._soLuongHang * 1);
+            return sum;
+        }
+
+        public void Remove(int id)
+        {
+            _listHang.RemoveAll(s => s.gioHang.IDMH == id);
+        }
+
+        public int Total()
+        {
+            return _listHang.Sum(s => s._soLuongHang);
+        }
+
+        public int TongTien()
+        {
+            int tongtien = 0;
+            foreach (Hang item in _listHang)
+            {
+                tongtien = tongtien + (int)(item.gioHang.DonGia * item._soLuongHang);
+            }
+            return tongtien;
+        }
+
+        public void clear()
+        {
+            _listHang.Clear();
+        }
+
+        public void ssss()
+        {
+            DateTime aDateTime = DateTime.Now;
+
+            Console.WriteLine("Now is " + aDateTime);
+
+            // Một khoảng thời gian. 
+            // 1 giờ + 1 phút
+            TimeSpan aInterval = new System.TimeSpan(0, 1, 1, 0);
+
+            // Thêm một khoảng thời gian.
+            DateTime newTime = aDateTime.Add(aInterval);
+        }
+
+         
+    }
+}
